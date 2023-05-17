33d4ebed046d11b4e00652d026e5f828fce3bca3
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-05-17 23:08:01 +0300

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FirstComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
