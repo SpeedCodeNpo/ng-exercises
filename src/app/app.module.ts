0afef7482b2d1335453b259fd03249a406415af9
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-05-27 18:23:15 +0300

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScrambleGameComponent } from './feature/scramble-game/scramble-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrambleGameComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/feature/scramble-game/scramble-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scramble-game',
+  templateUrl: './scramble-game.component.html',
+  styleUrls: ['./scramble-game.component.scss']
+})
+export class ScrambleGameComponent {
+
+}
