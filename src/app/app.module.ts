10ef8572518a01bfd7d273b3efa9c80f6c06f3d6
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-01 21:18:29 +0300

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardViewComponent } from './features/card-view/card-view.component';
 import { CardPageComponent } from './features/card-page/card-page.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardViewComponent,
-    CardPageComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule
-  ],
+  declarations: [AppComponent, CardPageComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
