b7d799d01d50a5600ade9e640ea84151b1c3bb87
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-01 10:33:00 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from './core/services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngExercise';
+  dataService = inject(DataService);
+
+  ngOnInit() {
+    console.log('Hello again');
+    this.dataService.dealCards(2);
+  }
 }
--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
-  constructor() { }
+  constructor() {}
 
   dzCards = [
     {
       titleName: 'Goku',
       titleHp: 1000,
-      imageUrl: 'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F1_goku_1000.png',
+      imageUrl:
+        'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F1_goku_1000.png',
       power1Name: 'Spirit Bomb',
       power1Points: 3000,
       power2Name: 'Kioken Finish',
@@ -22,7 +22,8 @@ export class DataService {
     {
       titleName: 'Goku',
       titleHp: 330,
-      imageUrl: 'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F2_goku_330.png',
+      imageUrl:
+        'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F2_goku_330.png',
       power1Name: 'Super Kamehameha',
       power1Points: 400,
       power2Name: 'Kamehameha',
@@ -33,7 +34,8 @@ export class DataService {
     {
       titleName: 'Kid Goku',
       titleHp: 180,
-      imageUrl: 'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F3_kid_goku_180.png',
+      imageUrl:
+        'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F3_kid_goku_180.png',
       power1Name: 'Kamehameha',
       power1Points: 100,
       power2Name: 'Power Pole Extend',
@@ -44,7 +46,8 @@ export class DataService {
     {
       titleName: 'Son Goku',
       titleHp: 150,
-      imageUrl: 'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F4_son_goku_150.png',
+      imageUrl:
+        'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F4_son_goku_150.png',
       power1Name: 'Meteor Crash',
       power1Points: 30,
       power2Name: 'Kamehameha',
@@ -55,15 +58,56 @@ export class DataService {
     {
       titleName: 'God Goku',
       titleHp: 200,
-      imageUrl: 'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F5_god_goku_200.png',
+      imageUrl:
+        'https://stackblitz.com/edit/ng-exercise1?file=src%2Fapp%2Fcore%2Fservices%2Fdata.service.ts,src%2Fassets%2Ffiles%2F5_god_goku_200.png',
       power1Name: 'Akiras Blessing',
       power1Points: 10,
       power2Name: 'Main Protagonist',
       power2Points: 10,
       power3Name: 'Aurora Stream',
       power3Points: 40,
+    },
+  ];
+
+  shuffle(values: any[]) {
+    let index = values.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (index != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * index);
+      index--;
+
+      // And swap it with the current element.
+      //Destructuring assignment allows you to unpack the values from an array into multiple variables. In this case, the destructuring assignment is unpacking the values from the array [values[index], values[randomIndex]] into the variables values[index] and values[randomIndex].
+      [values[index], values[randomIndex]] = [
+        values[randomIndex],
+        values[index],
+      ];
     }
-  ]
 
+    return values;
+  } //shuffle
 
+  dealCards(quantity: number) {
+    const deckLength = this.dzCards.length;
+    if (quantity > deckLength) {
+      quantity = deckLength;
+    }
+
+    console.log('quantity = ' + quantity);
+
+    const myTest = ['a', 'b', 'c', 'd'];
+    let dealtCards = this.shuffle(myTest); //this.dzCards);
+
+    console.log(`dealtCards[]= ${dealtCards}`);
+    // for (let i = 1; i <= quantity; i++) {
+    //   console.log('i= ' + i);
+
+    //   dealtCards.push(this.dzCards[i - 1]);
+    // } //for
+
+    // console.log(`dealtCards[]= ${JSON.stringify(dealtCards)}`);
+  } //dealCards
 }
