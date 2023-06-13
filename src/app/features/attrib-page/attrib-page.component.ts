f8b0507fbdeff0ccdcb4cd64f61051624ed7e8b3
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-13 17:30:22 +0300

    4 files modified

--- src/app/features/attrib-page/attrib-page.component.ts
+++ src/app/features/attrib-page/attrib-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-attrib-page',
   templateUrl: './attrib-page.component.html',
-  styleUrls: ['./attrib-page.component.scss']
+  styleUrls: ['./attrib-page.component.scss'],
 })
 export class AttribPageComponent {
+  defaultAlignment = 'left';
+  alignRequested = this.defaultAlignment;
 
+  onClickAlign(alignValue: string) {
+    if (alignValue !== 'left' || 'center' || 'right' || 'justify') {
+      this.alignRequested = this.defaultAlignment;
+    }
+    this.alignRequested = alignValue;
+  }
 }
