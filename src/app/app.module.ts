cfc49753042924028b32541d8925f2ef1fd85d50
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-13 17:02:34 +0300

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AttribPageComponent } from './features/attrib-page/attrib-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttribPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/features/attrib-page/attrib-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attrib-page',
+  templateUrl: './attrib-page.component.html',
+  styleUrls: ['./attrib-page.component.scss']
+})
+export class AttribPageComponent {
+
+}
