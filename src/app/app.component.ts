ab33576e34666f23fc921d80bffbf9a41e4a49cd
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-16 22:28:05 +0300

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StudentDataService } from './core/services/student-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngExercise';
