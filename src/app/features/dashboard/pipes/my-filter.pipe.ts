185182cbc6f546330cb414d2e1ffe46a4fa28add
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-17 15:44:17 +0300

    1 file modified

--- src/app/features/dashboard/pipes/my-filter.pipe.ts
+++ src/app/features/dashboard/pipes/my-filter.pipe.ts
@@ -1,8 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { filter } from 'rxjs';
 
 @Pipe({
   name: 'myFilter',
+  //Regarding the "pure" sttribute, 
+  // (1) The default is 'true' this means "pure pipe"
+  //     pure means that the pipe is minded to best performance,
+  //     that means is does not do change-detection for complex types
+  //     such as properties of objects. It will do it for primitive types.
+  // (2) If you set the value to 'false' that means that
+  //     performance will be lower but you get full change detection.
+  //     This means if a value inside a complex object changes, then 
+  //     the filter will refresh the calculation.
+  //
   pure: false,
 })
 export class MyFilterPipe implements PipeTransform {
