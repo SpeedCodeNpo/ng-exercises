c3bcf9222039a71797be42be4bc03239d56d5e71
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-17 12:29:20 +0300

    2 files modified, 3 added

---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { StudentDataService } from 'src/app/core/services/student-data.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  studentService = inject( StudentDataService);
+  studentInfo = this.studentService.students;
+
+}
