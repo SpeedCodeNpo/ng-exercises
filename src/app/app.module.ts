c09032529e31528f90943fe4d4363301f27a6a3c
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-21 12:44:13 +0300

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DogDashboardComponent } from './features/dog-dashboard/dog-dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, DogDashboardComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
