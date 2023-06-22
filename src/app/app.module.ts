682d3f4bfe54545c0605bac412b4e90879221c28
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-22 17:51:18 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyPageComponent } from './features/my-page/my-page.component';
-import { TimesDirective } from './core/directives/duplicat.directive';
+import { MultiplierDirective } from './core/directives/duplicat.directive';
 
 @NgModule({
-  declarations: [AppComponent, MyPageComponent, TimesDirective],
+  declarations: [AppComponent, MyPageComponent, MultiplierDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
