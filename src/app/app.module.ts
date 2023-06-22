897f1c0961544b9e7a3898da32ed1fb2c7e1b0b2
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-22 17:55:06 +0300

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MultiplierDirective } from './core/directives/multiplier.directive';
 import { MyPageComponent } from './features/my-page/my-page.component';
-import { MultiplierDirective } from './core/directives/duplicat.directive';
+
+
 
 @NgModule({
   declarations: [AppComponent, MyPageComponent, MultiplierDirective],
