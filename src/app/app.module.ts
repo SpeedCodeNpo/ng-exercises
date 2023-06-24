0e9447cff2db1c668b5f4c114c22708d070d29b1
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-24 15:06:05 +0300

    1 file modified, 6 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app/features/home/home.component';
+import { AnotherComponent } from './features/another/another.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, AnotherComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
