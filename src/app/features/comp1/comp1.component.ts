ad5d8d9837619044e910e23df351fc3124d9cff1
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-06-29 23:04:46 +0300

    3 files modified

--- src/app/features/comp1/comp1.component.ts
+++ src/app/features/comp1/comp1.component.ts
@@ -9,8 +9,21 @@ export class Comp1Component {
   initialCount = 0;
   count = signal<number>(this.initialCount);
 
+  userInfo = signal({
+    name: 'Danny',
+    age: 27,
+    email: 'my@email.com',
+  });
+
+  //FYI: If the default value of the ngModel property 'username' ia anythoing but
+  //an empty string, then in the <input> tag in the html template the placeholder value will be ignored and the ngModel value of 'userbname' will be displpayed in the input box.
+  username = '';
+
   consoleLogCount() {
-    console.log(`The signal 'count' value is : ${this.count()}`);
+    //In signal, the GET is activated by caliing the property name with () parenthesis.
+    console.log(
+      `Using signal GET to see it's value. Value of 'count' : ${this.count()}`
+    );
   }
 
   resetCount() {
@@ -22,5 +35,9 @@ export class Comp1Component {
       value++;
       return value;
     });
+  }
+
+  renameUser() {
+    this.userInfo.mutate((v) => (v.name = this.username));
   }
 }
