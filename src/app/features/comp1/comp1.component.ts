65c26a26d084b1b0419147c5d08a4654689b655c
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-05 21:20:16 +0300

    3 files modified

--- src/app/features/comp1/comp1.component.ts
+++ src/app/features/comp1/comp1.component.ts
@@ -6,47 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-  keyName = '';
-  keyValue = '';
-  storageContentList: {}[] = [];
+  requestedKeyName = '';
+  requestedKeyValue = '';
+  //storageContentList: {}[] = [];
+  numberOfKeysInStorage = Object.keys(localStorage).length;
 
   setInLocalStorage() {
-    const nameToSet = this.keyName ? this.keyName : 'noKeyName';
-    const valueToSet = this.keyValue ? this.keyValue : 'noKeyValue';
+    const nameToSet = this.requestedKeyName
+      ? this.requestedKeyName
+      : 'noKeyName';
+    const valueToSet = this.requestedKeyValue
+      ? this.requestedKeyValue
+      : 'noKeyValue';
 
     localStorage.setItem(nameToSet, valueToSet);
-    this.storageContentList.push({ nameToSet, valueToSet });
   }
 
-  getStorageContent() {
-    //We first clean up our current content list
-    this.storageContentList = [];
-
-    //Loop through all the keys and get each one's value
-    let keys = Object.keys(localStorage);
-    for (let key of keys) {
-      let value = localStorage.getItem(key);
-      if (!value) {
-        value = 'no-value';
-      }
-
-      //Now adding the key & va;ue into the array.
-      this.storageContentList.push({ key: value });
-      console.log('+ ');
+  getValueOfKey() {
+    //let keys = Object.keys(localStorage);
+    const value = localStorage.getItem(this.requestedKeyName);
+    if (!value) {
+      this.requestedKeyValue = 'no-value';
+    } else {
+      this.requestedKeyValue = value;
     }
+  }
 
-    if (this.storageContentList.length === 0) {
-      console.log('-------- empty');
-    }
-    for (let item of this.storageContentList) {
-      console.log('+ ' + JSON.stringify(item));
-    }
+  removeKeyValue() {
+    localStorage.removeItem(this.requestedKeyName);
+    this.requestedKeyName = '';
+    this.requestedKeyValue = '';
   }
 
   clearAllLocalStorage() {
     localStorage.clear();
-    this.keyName = '';
-    this.keyValue = '';
-    this.storageContentList = [];
+    this.requestedKeyName = '';
+    this.requestedKeyValue = '';
   }
 }
