53ad10372af059e0fd466efc622e9dc5d082a9c7
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-07 22:26:28 +0300

    2 files modified

--- src/app/features/comp1/comp1.component.ts
+++ src/app/features/comp1/comp1.component.ts
@@ -12,25 +12,6 @@ export class Comp1Component {
   //storageContentList: {}[] = [];
   numberOfKeysInStorage = Object.keys(localStorage).length;
 
-  cryptoStore = new CryptoStorage('my crypto key');
-
-  async storeCrypto() {
-    // Secure store your data locally fully encrypted
-    const originalValue = 'any data value';
-    await this.cryptoStore.set('baba', originalValue);
-    await this.cryptoStore.set('data key3', 'la la la');
-    console.log('data was encrypted');
-    // Look at the browser dev tools from IndexedDb,
-    // you will not be able to read the data key or value.
-  }
-  async decryptCrypto() {
-    // Then, retrieve your original data decrypted again:
-    const decryptedValue = await this.cryptoStore.get('data key');
-    console.log('The decrypted value is "' + decryptedValue + '"');
-    const decryptedValue3 = await this.cryptoStore.get('data key3');
-    console.log('The decrypted3 value is "' + decryptedValue3 + '"');
-  }
-
   setInLocalStorage() {
     const nameToSet = this.requestedKeyName
       ? this.requestedKeyName
