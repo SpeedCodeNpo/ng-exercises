7858177cd382e28b49648c65c56cbccf449e4a7e
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-11 18:00:20 +0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './app/features/comp1/comp1.component';
+import { AtomButtonComponent } from './app/core-view/components/atoms/atom-button/atom-button.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Comp1Component, AtomButtonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
