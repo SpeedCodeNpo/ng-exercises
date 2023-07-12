e08b79c71c8a250d47cd786e13b7c07d4ed6aaf3
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-12 12:42:07 +0300

    1 file modified

--- src/app/core-view/components/atoms/atom-button/atom-button.component.ts
+++ src/app/core-view/components/atoms/atom-button/atom-button.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'atom-button',
   templateUrl: './atom-button.component.html',
   styleUrls: ['./atom-button.component.scss'],
 })
-export class AtomButtonComponent {}
+export class AtomButtonComponent {
+  @Input() text!: string;
+}
