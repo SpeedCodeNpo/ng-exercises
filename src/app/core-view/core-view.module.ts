b15b25f855926f26758305f31fb43bfa0a4f1e50
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-12 12:35:50 +0300

    1 file modified

--- src/app/core-view/core-view.module.ts
+++ src/app/core-view/core-view.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AtomButtonComponent } from './components/atoms/atom-button/atom-button.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AtomButtonComponent],
   imports: [
     CommonModule
-  ]
+  ], exports: [ AtomButtonComponent]
 })
 export class CoreViewModule { }
