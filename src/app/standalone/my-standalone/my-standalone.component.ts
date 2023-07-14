7195744dad81b078b5002dc66f00cbdee335b9af
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-14 21:05:23 +0300

    3 files modified, 4 added

--- src/app/standalone/my-standalone/my-standalone.component.ts
+++ src/app/standalone/my-standalone/my-standalone.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Comp2Component } from 'src/app/features/comp2/comp2.component';
 
 @Component({
   selector: 'app-my-standalone',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Comp2Component],
   templateUrl: './my-standalone.component.html',
   styleUrls: ['./my-standalone.component.scss']
+
 })
 export class MyStandaloneComponent {
 
