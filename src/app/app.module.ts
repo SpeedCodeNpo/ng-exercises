fe8093be7705b99f4d5b9b75e05c747746a7fc28
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-18 23:01:29 +0300

    2 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './pages/comp1/comp1.component';
+import { Comp2Component } from './pages/comp2/comp2.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Comp1Component, Comp2Component, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
