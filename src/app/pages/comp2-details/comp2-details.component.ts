5fc6f405d7b9c4caac82efe94ab3dca488a4d457
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-20 23:27:23 +0300

    4 files modified

--- src/app/pages/comp2-details/comp2-details.component.ts
+++ src/app/pages/comp2-details/comp2-details.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-comp2-details',
   templateUrl: './comp2-details.component.html',
-  styleUrls: ['./comp2-details.component.scss']
+  styleUrls: ['./comp2-details.component.scss'],
 })
 export class Comp2DetailsComponent {
+  public receivedId: string | null = 'none';
+  route = inject(ActivatedRoute);
 
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.receivedId = id;
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  router = inject(Router);
 
+  onSelect(message: string) {
+    this.router.navigate(['/comp2', message]);
+  }
 }
