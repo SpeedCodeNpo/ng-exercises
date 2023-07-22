a4337e1b7ea6663d36219360a04a10826af31d9d
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-22 15:35:49 +0300

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes , RouterModule}  from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
 import { Comp1Component } from './pages/comp1/comp1.component';
 import { Comp2Component } from './pages/comp2/comp2.component';
 import { Comp2DetailsComponent } from './pages/comp2-details/comp2-details.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes : Routes = [
-  {path: 'comp1', component: Comp1Component},
-  {path: 'comp2', component: Comp2Component},
-  {path: 'comp2/:id', component: Comp2DetailsComponent},
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'comp1', component: Comp1Component },
+  { path: 'comp2', component: Comp2Component },
+  { path: 'comp2/:id', component: Comp2DetailsComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule] 
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
