3f9ca11a91c4ce1750b334b2766a45591b603479
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-07-22 18:16:01 +0300

    4 files modified

--- README.md
+++ README.md
@@ -58,6 +58,20 @@ https://github.com/SpeedCodeNpo/ng-exercises/tree/card-20230717-2303-s
 ### Send params using Route Parameter
 - Inside comp1 create a route url that passes the word "Hello" to comp2.
 
+
+### There are 2 tasks in this assignment for UX :
+ This exrecise has two tasks:
+ ### task-1 : Have the routed components displayed on the same page (home):
+          (1) When click on link then the link text uses the ".activeFlag" css
+              class to highlight the current active route chosen.
+          (2) The component will be displayed on the same page, it replaces
+               the previous component that was displayed (1 or 2).
+
+ ### task-2 : Have the routed components display their own page.
+          (1) When click on link then the home page disapers and the component (1 or 2)
+              are displayed on their own page.
+          (2) The page displayed needs to have a link back to the home page.
+
 ### Screenshot of the page before user input :
 
 - ![image](https://github.com/SpeedCodeNpo/ng-exercises/assets/132397719/e8e959ba-ffd6-4a8b-93cb-f46b1798d57d)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { Comp2Component } from './pages/comp2/comp2.component';
 import { Comp2DetailsComponent } from './pages/comp2-details/comp2-details.component';
 import { HomeComponent } from './pages/home/home.component';
 
-
-
-
+// This exrecise has two tasks:
+// task-1 : Have the routed components displayed on the same page (home):
+//          (1) When click on link then the link text uses the ".activeFlag" css
+//              class to highlight the current active route chosen.
+//          (2) The component will be displayed on the same page, it replaces
+//               the previous component that was displayed (1 or 2).
+//
+// task-2 : Have the routed components display their own page.
+//          (1) When click on link then the home page disapers and the component (1 or 2)
+//              are displayed on their own page.
+//          (2) The page displayed needs to have a link back to the home page.
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // comment-out in task-2 of this exercise
+  // { path: '', component: HomeComponent }, // comment-out in task-2 of this exercise
   { path: 'comp1', component: Comp1Component },
   { path: 'comp2', component: Comp2Component },
   { path: 'comp2/:id', component: Comp2DetailsComponent },
