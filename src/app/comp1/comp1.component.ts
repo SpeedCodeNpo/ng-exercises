bb348841de223fc7cb74b6bbf33759346b56e688
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-09 15:33:22 +0300

    2 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, Observer, TeardownLogic } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
@@ -7,78 +7,79 @@ import { Observable, Observer, TeardownLogic } from 'rxjs';
   styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-  isActivateComplete: boolean = false;
+  isActivateComplete: boolean = true;
   asyncEmissionTimeMillisec = 2000;
-
-  mySubscribeFunction(
-    someObserver: Observer<number>,
-    actiavteComplete: boolean,
-    emissionTimeout: number
-  ) {
-    console.log('Executing the mySubscribeFunction()');
-    someObserver.next(1);
-
-    setTimeout(() => someObserver.next(2), emissionTimeout);
-    setTimeout(() => someObserver.next(3), 2 * emissionTimeout);
-
-    console.log(`Value of this.isActivateComplete is : "${actiavteComplete}"`);
-    if (actiavteComplete) {
-      someObserver.complete();
-    } else {
-      someObserver.error(new Error(`Ending subscription with an error.`));
-    }
-
-    return () => {
-      console.log('Executing the teardown.');
-    };
-  }
-
-  myObserver1 = {
-    next: (value: number) => console.log(`Value of the emission is "${value}"`),
-    complete: () => console.log(`Triggered a COMPLETE.`),
-    error: (myError: Error) =>
-      console.log(`Triggered an error, the message is : "${myError}"`),
-  };
-
-  myObserver2 = {
-    next: (value: number) =>
-      console.log(`Observer2: Value of the emission is "${value}"`),
-    complete: () => console.log(`Observer2: Triggered a COMPLETE.`),
-    error: (myError: Error) =>
-      console.log(
-        `Observer2: Triggered an error, the message is : "${myError}"`
-      ),
-  };
 
   ////////////////////////////
 
   ngOnInit() {
     console.log(
-      '===== ngOnInit : Creating a new Observable instance : myObservable$, this will already activate it.'
-    );
-    const myObservable1$ = new Observable(
-      this.mySubscribeFunction(
-        this.myObserver1,
-        this.isActivateComplete,
-        this.asyncEmissionTimeMillisec
-      )
+      '===== ngOnInit : Creating a new Observable instance : myObservable$.'
     );
 
-    const myObservable2$ = new Observable(
-      this.mySubscribeFunction(
-        this.myObserver2,
-        this.isActivateComplete,
-        this.asyncEmissionTimeMillisec
-      )
+    const myObservable$ = new Observable((mySubscriber) => {
+      console.log('myObservable$: Executing the creation of myObservable$');
+      mySubscriber.next(1);
+
+      setTimeout(() => mySubscriber.next(2), this.asyncEmissionTimeMillisec);
+      setTimeout(
+        () => mySubscriber.next(3),
+        2 * this.asyncEmissionTimeMillisec
+      );
+
+      // console.log(
+      //   `myObservable$: Value of this.isActivateComplete is : "${this.isActivateComplete}"`
+      // );
+      if (this.isActivateComplete) {
+        mySubscriber.complete();
+      } else {
+        mySubscriber.error(
+          new Error(`myObservable$: Ending subscription with an error.`)
+        );
+      }
+
+      return () => {
+        console.log('myObservable$: Executing the teardown.');
+      };
+    });
+
+    //////////////////////////////////////
+
+    console.log(
+      '===== ngOnInit : BEFORE subscribing to myObservable$, creating variable mySubscription.'
     );
 
-    // console.log(
-    //   '===== ngOnInit : Subscribing to myObservable$, creating variable mySubscription.'
+    const mySubscription1 = myObservable$.subscribe({
+      next: (value) =>
+        console.log(`Observer1: Value of the emission is "${value}"`),
+
+      complete: () => console.log(`Observer1: Triggered a COMPLETE.`),
+
+      error: (myError: Error) =>
+        console.log(
+          `Observer1: Triggered an error, the message is : "${myError}"`
+        ),
+    });
+    console.log('===== ngOnInit : AFTER subscribing to myObservable$.');
+
+    // const mySubscription2 = myObservable$.subscribe({
+    //   next: (value) =>
+    //     console.log(`Observer2: Value of the emission is "${value}"`),
+
+    //   complete: () => console.log(`Observer2: Triggered a COMPLETE.`),
+
+    //   error: (myError: Error) =>
+    //     console.log(
+    //       `Observer2: Triggered an error, the message is : "${myError}"`
+    //     ),
+    // });
+
+    // console.log('===== ngOnInit : BEFORE unsubscribing from mySubscription1.');
+    // setTimeout(
+    //   () => mySubscription1.unsubscribe(),
+    //   3 * this.asyncEmissionTimeMillisec
     // );
-    // const mySubscription = myObservable$.subscribe();
-
-    // mySubscription.unsubscribe();
-    // console.log('===== ngOnInit : Unsubscribed from mySubscription.');
-    //this.mySubscribeFunction(this.myObserver);
+    // // mySubscription1.unsubscribe();
+    // console.log('===== ngOnInit : AFTER Unsubscribed from mySubscription1.');
   } //ngOnInit
 }
