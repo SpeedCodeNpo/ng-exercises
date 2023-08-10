09c40e4e535f5dbd526d08fda71620033cabe99b
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-10 22:42:39 +0300

    1 file modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
-  styleUrls: ['./comp1.component.scss']
+  styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-
+  asyncEmissionTimeMillisec = 2000;
+  isNeverEnding: boolean = false;
+  isActivateComplete: boolean = true;
 }
