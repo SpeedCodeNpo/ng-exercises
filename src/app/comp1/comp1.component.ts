b0f52863bb01730e1004acc8d07172ffcb835827
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-13 10:29:20 +0300

    2 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-  asyncEmissionTimeMillisec = 2000;
-  isNeverEnding: boolean = false;
-  isActivateComplete: boolean = true;
+  myOfObservable$ = of(1, 2, 3);
+  onClickOf() {
+    // Subscribe to the of Observable
+    console.log('Before subscribing to the of Observable');
+    this.myOfObservable$.subscribe((value) => {
+      console.log(`Value is ${value}`);
+    });
+    console.log('AFTER subscribing to the of Observable');
+  }
 }
