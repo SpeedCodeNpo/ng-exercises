19b2442dff8e6ee999ca0952bc5d34b41236c94c
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-13 18:16:59 +0300

    2 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { Observable, Subscription, from, fromEvent, of, timer } from 'rxjs';
+import {
+  Observable,
+  Subscription,
+  from,
+  fromEvent,
+  interval,
+  of,
+  timer,
+} from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
@@ -30,8 +38,14 @@ export class Comp1Component {
   // Setting values for the "timer" Observable
   //----------------------------------------
   timerLengthMilisec = 5000;
-  timerCounter = 0;
   isTimerActive = false;
+
+  //----------------------------------------
+  // Setting values for the "interval" Observable
+  //----------------------------------------
+  intervalTimeMilisec = 1000;
+  intervalCounter = 0;
+  isIntervalActive = false;
 
   //=============================================
   // Functions
@@ -99,7 +113,6 @@ export class Comp1Component {
   //----------------------------------------
   // Defining the function for clicking on "Set Timer" button
   //----------------------------------------
-
   onClickSetTimer() {
     if (!this.isTimerActive) {
       this.isTimerActive = true;
@@ -114,5 +127,26 @@ export class Comp1Component {
         },
       });
     }
-  }
+  } // onClickSetTimer
+
+  //----------------------------------------
+  // Defining the function for clicking on "Set Timer" button
+  //----------------------------------------
+  onClickSetInterval() {
+    let myIntervalSubscription = new Subscription();
+    if (!this.isIntervalActive) {
+      this.isIntervalActive = true;
+      const myIntervalObservable$ = interval(this.intervalTimeMilisec);
+      myIntervalSubscription = myIntervalObservable$.subscribe({
+        next: () => {
+          console.log(`interval counter = ${this.intervalCounter}`);
+          this.intervalCounter++;
+          if (this.intervalCounter > this.timerLengthMilisec / 1000) {
+            this.isIntervalActive = false;
+            myIntervalSubscription.unsubscribe();
+          } //if this.intervalCounter > limit
+        },
+      });
+    } //if isIntervalActive
+  } //onClickSetInterval
 } //end comp1
