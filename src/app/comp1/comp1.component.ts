51dffc36e8a55d0f3f1605eeaf1dfd4a7fc94dfc
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-14 15:41:45 +0300

    2 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subscription, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
@@ -6,7 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-  asyncEmissionTimeMillisec = 2000;
-  isNeverEnding: boolean = false;
-  isActivateComplete: boolean = true;
+  //----------------------------------------
+  // Setting values for the "of" Observable
+  //----------------------------------------
+  myOfObservable$ = of(1, 2.3256, 'Hello', ['a', 'b'], {
+    prop1: 'value1',
+    prop2: 'value2',
+  });
+
+  //----------------------------------------
+  // Setting values for the "from" Observable
+  //----------------------------------------
+  myFromObservable$ = from(['One', 'Two', 'Three']);
+
+  //----------------------------------------
+  // Setting values for the "fromEvent" Observable
+  //----------------------------------------
+  isSubscribedToEvent = false;
+  myFromEventSubscription = new Subscription();
+
+  //----------------------------------------
+  // Defining the function for clicking on the "of" button
+  //----------------------------------------
+  onClickOf() {} //onClickOf
+
+  //----------------------------------------
+  // Defining the function for clicking on the "from" button
+  //----------------------------------------
+  onClickFrom() {} //onClickFrom
+
+  //----------------------------------------
+  // Defining the function for clicking on the "from" button
+  //----------------------------------------
+  onClickFromEvent() {} //onClickFromEvent
+
+  onClickUnsubscribeFromEvent() {} //onClickUnsubscribeFromEvent
 }
