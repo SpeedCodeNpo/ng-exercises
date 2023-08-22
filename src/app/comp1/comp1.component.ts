2efce8b1764296dcc3604947450393384f2bb0bd
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-22 16:52:41 +0300

    2 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,104 +1,29 @@
-import { Component } from '@angular/core';
-import {
-  BehaviorSubject,
-  Subject,
-  Subscription,
-  interval,
-} from 'rxjs';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.scss'],
 })
-export class Comp1Component {
-  //----------------------------------------
-  // Setting values for the "Subject"or "BehaviorSubject" Observable
-  //----------------------------------------
-
-  //++++++++++
-  //+++ To demo the BehaviorSubject uncomment below
-  //++++++++++
-  //mySubject$ = new BehaviorSubject(33); // Argument is arbitrary & not used.
-
-  //++++++++++
-  //+++ To demo the Subject uncomment below
-  //++++++++++
-  mySubject$ = new Subject();
-
-  // Class variables
-  intervalEmiter$ = interval(2000); //milisecs
-
-  intervalEmiterSubscribe$ = this.intervalEmiter$.subscribe(
-    (intervalCount: number) => {
-      this.mySubject$.next(intervalCount);
-      this.subjectValue = intervalCount;
-    }
-  );
-
-  subjectValue = 0;
-  isSubjectActive = true; //Subject is activated automatically
-
-  //Value to identify a counter in state of unsubscribed
-  UNSUBSCRIBED_START_VALUE = -1010; 
-
-  // Class variables for subscription-1
-  mySubscribe1 = new Subscription();
-  isSubscribe1Active = false;
-  subscribe1Value = this.UNSUBSCRIBED_START_VALUE;
-
-  // Class variables for subscription-2
-  mySubscribe2 = new Subscription();
-  isSubscribe2Active = false;
-  subscribe2Value = this.UNSUBSCRIBED_START_VALUE;
+export class Comp1Component implements OnInit {
+  isGetUserActive = true;
+  userFullName = 'No User';
 
   //=============================================
   // Functions
   //=============================================
 
   /**
-   * Function to unsubscribe from the Subject or BehaviorSubject
+   * Function to get user data
    */
-  onClickSubjectUnsubscribe() {
-    this.intervalEmiterSubscribe$.unsubscribe();
-    this.isSubjectActive = false;
+  onClickGetUser() {} //onClickGetUser
+
+  ngOnInit() {
+    console.log('======== START ngOnInit  ==========');
   }
 
-  /**
-   * Function of first subscription to Subject or BehaviorSubject
-   */
-  onClickSubscribe1() {
-    this.isSubscribe1Active = true;
-    this.mySubscribe1 = this.mySubject$.subscribe((value) => {
-      this.subscribe1Value = value as number;
-      console.log('Subscribe-1 counter : ', value);
-    });
-  } // onClickSubscribe1
-
-   /**
-   * Function unsubscribe from first subscription
-   */
-  onClickUnsubscribe1() {
-    this.isSubscribe1Active = false;
-    this.mySubscribe1.unsubscribe();
-    this.subscribe1Value = this.UNSUBSCRIBED_START_VALUE;
-  } // onClickUnsubscribe1
-
-   /**
-   * Function of second subscription to Subject or BehaviorSubject
-   */
-  onClickSubscribe2() {
-    this.isSubscribe2Active = true;
-    this.mySubscribe2 = this.mySubject$.subscribe((value) => {
-      this.subscribe2Value = value as number;
-      console.log('Subscribe-2 counter : ', value);
-    });
-  } // onClickSubscribe2
-
-   /**
-   * Function unsubscribe from second subscription
-   */
-  onClickUnsubscribe2() {
-  } // onClickUnsubscribe2
-
+  ngOnDestroy() {
+    console.log('======== Unsubscribed  ==========');
+  }
 } //end comp1
