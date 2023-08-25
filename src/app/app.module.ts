47fe007c3c89ed7975efc8be90e980519a29e4ec
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-25 17:18:57 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [AppComponent, Comp1Component],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -8,46 +8,10 @@ import { UserInterface } from '../interface/user';
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.scss'],
 })
-export class Comp1Component implements OnInit {
-  isGetUserActive = true;
-  userFullName = 'No User';
-  userFound!: UserInterface | null;
-  httpService = inject(HttpService);
-  getUserSubscription = new Subscription();
+export class Comp1Component {
+  selectedModifiedStyle = 'state-normal'; //Other options : state-success and state-warn
 
   //=============================================
   // Functions
   //=============================================
-
-  /**
-   * Function to get user data
-   */
-  onClickGetUser() {
-    const myUser = this.httpService.getUser();
-    this.isGetUserActive = false;
-    this.getUserSubscription = myUser.subscribe({
-      next: (response) => {
-        this.isGetUserActive = true;
-        this.userFound = response;
-        console.log(`...... GOT A USER  .....`);
-        const firstUser = response.results[0];
-        this.userFullName = `${firstUser.name.title} ${firstUser.name.first} ${firstUser.name.last}`;
-        console.log(`>> The user is : ${this.userFullName}`);
-        return response;
-      },
-
-      error: () => {
-        console.log('.......... Error in getUser');
-        this.userFound = null;
-      },
-    });
-  } //onClickGetUser
-
-  ngOnInit() {
-    console.log('======== START ngOnInit  ==========');
-  }
-
-  ngOnDestroy() {
-    this.getUserSubscription.unsubscribe();
-  }
 } //end comp1
