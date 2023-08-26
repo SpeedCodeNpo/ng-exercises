71c3bd9681c7d267488b7ed2685b54bcb156e876
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-26 14:51:07 +0300

    5 files modified

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit, inject } from '@angular/core';
   styleUrls: ['./comp1.component.scss'],
 })
 export class Comp1Component {
-  selectedModifiedStyle = 'button-basic__ button-basic__--state-normal'; //Other options : state-success and state-warn
+  selectedModifiedStyle = 'btn-basic__ btn-basic__--state-normal'; //Other options : state-success and state-warn
 
   //=============================================
   // Functions
