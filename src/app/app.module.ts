edda3b37a1193a2402b761ab166385dc8bb15786
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-08-30 15:41:47 +0300

    8 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, Comp1Component],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,19 +1,12 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import { HttpService } from '../http-service/http-service.service';
-import { UserInterface } from '../interface/user';
 
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.scss'],
 })
-export class Comp1Component implements OnInit {
-  isGetUserActive = true;
-  userFullName = 'No User';
-  userFound!: UserInterface | null;
-  httpService = inject(HttpService);
-  getUserSubscription = new Subscription();
+export class Comp1Component {
+  selectedModifiedStyle = 'mat-raised-button color="accent"';
 
   //=============================================
   // Functions
@@ -22,32 +15,4 @@ export class Comp1Component implements OnInit {
   /**
    * Function to get user data
    */
-  onClickGetUser() {
-    const myUser = this.httpService.getUser();
-    this.isGetUserActive = false;
-    this.getUserSubscription = myUser.subscribe({
-      next: (response) => {
-        this.isGetUserActive = true;
-        this.userFound = response;
-        console.log(`...... GOT A USER  .....`);
-        const firstUser = response.results[0];
-        this.userFullName = `${firstUser.name.title} ${firstUser.name.first} ${firstUser.name.last}`;
-        console.log(`>> The user is : ${this.userFullName}`);
-        return response;
-      },
-
-      error: () => {
-        console.log('.......... Error in getUser');
-        this.userFound = null;
-      },
-    });
-  } //onClickGetUser
-
-  ngOnInit() {
-    console.log('======== START ngOnInit  ==========');
-  }
-
-  ngOnDestroy() {
-    this.getUserSubscription.unsubscribe();
-  }
 } //end comp1
