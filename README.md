4de2c47d3185372c82c14a89320256a91f58745d
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-09-02 12:04:26 +0300

    2 files modified, 3 deleted

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Ng Exercises
 
 Welcome to Speed Code Exercises
-Solution version for assignment: card-20230830-1100
+Starter version for assignment: card-20230830-1100
 Subject: Angular Material
 
 ## Assignment title : Practice RxJs Subject and BehaviorSubject
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './core-view/material.module';
 
 @NgModule({
   declarations: [AppComponent, Comp1Component],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MaterialModule
-  ],
+  imports: [BrowserModule, FormsModule],
   exports: [],
   providers: [],
   bootstrap: [AppComponent],
