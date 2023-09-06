b0c70b977de44e67c567d421c8a59d4ee26a7bcc
SpeedCodeNpo <132397719+SpeedCodeNpo@users.noreply.github.com>
2023-09-06 22:27:45 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
@@ -8,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, Comp1Component],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
